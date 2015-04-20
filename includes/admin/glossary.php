970ec20ffbeaa75eb9a26f6344002fdb4ba3b42c
emarket-design <emarket-design@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-20 17:19:21 +0000

    version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/employee-spotlight/tags/1.0.0@1139315 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin/glossary.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Settings Glossary Functions
+ *
+ * @package EMPSLIGHT_COM
+ * @version 1.0.0
+ * @since WPAS 4.0
+ */
+if (!defined('ABSPATH')) exit;
+add_action('empslight_com_settings_glossary', 'empslight_com_settings_glossary');
+/**
+ * Display glossary information
+ * @since WPAS 4.0
+ *
+ * @return html
+ */
+function empslight_com_settings_glossary() {
+	global $title;
+?>
+<div class="wrap">
+<h2><?php echo $title; ?></h2>
+<p><?php _e('Employee Spotlight displays photo, bio, and contact information of your employees, founders, team or just yourself. Each employee has its own page.', 'empslight-com'); ?></p>
+<p><?php _e('The below are the definitions of entities, attributes, and terms included in Employee Spotlight.', 'empslight-com'); ?></p>
+<div id="glossary" class="accordion-container">
+<ul class="outer-border">
+<li id="emd_employee" class="control-section accordion-section">
+<h3 class="accordion-section-title hndle" tabindex="1"><?php _e('Employees', 'empslight-com'); ?></h3>
+<div class="accordion-section-content">
+<div class="inside">
+<table class="form-table"><p class"lead"><?php _e('Employees are human resources that work for your organization. Employees can be identified as staff, team members, founders, or contractors.', 'empslight-com'); ?></p><tr>
+<th><?php _e('Featured', 'empslight-com'); ?></th>
+<td><?php _e('Sets employee as featured which can be used to select employees in available views using Visual Shortcode Builder and Featured employee widget. Featured does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Photo', 'empslight-com'); ?></th>
+<td><?php _e('Photo of the employee. 250x250 is the preferred size. Photo does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Job Title', 'empslight-com'); ?></th>
+<td><?php _e(' Job Title does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Primary Address', 'empslight-com'); ?></th>
+<td><?php _e(' Primary Address does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Phone', 'empslight-com'); ?></th>
+<td><?php _e(' Phone is filterable in the admin area. Phone does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Mobile', 'empslight-com'); ?></th>
+<td><?php _e(' Mobile is filterable in the admin area. Mobile does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Email', 'empslight-com'); ?></th>
+<td><?php _e(' Email is a required field. Being a unique identifier, it uniquely distinguishes each instance of Employee entity. Email does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Facebook', 'empslight-com'); ?></th>
+<td><?php _e(' Facebook does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Google+', 'empslight-com'); ?></th>
+<td><?php _e(' Google+ does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Twitter', 'empslight-com'); ?></th>
+<td><?php _e(' Twitter does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Linkedin', 'empslight-com'); ?></th>
+<td><?php _e(' Linkedin does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Github', 'empslight-com'); ?></th>
+<td><?php _e(' Github is filterable in the admin area. Github does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Bio', 'empslight-com'); ?></th>
+<td><?php _e(' Bio does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Full Name', 'empslight-com'); ?></th>
+<td><?php _e(' Full Name is a required field. Full Name is filterable in the admin area. Full Name does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Excerpt', 'empslight-com'); ?></th>
+<td><?php _e(' Excerpt does not have a default value. ', 'empslight-com'); ?></td>
+</tr><tr>
+<th><?php _e('Group', 'empslight-com'); ?></th>
+
+<td><?php _e(' Group accepts multiple values like tags', 'empslight-com'); ?>. <?php _e('Group does not have a default value', 'empslight-com'); ?>.<div class="taxdef-block"><p><?php _e('There are no preset values for <b>Group:</b>', 'empslight-com'); ?></p></div></td>
+</tr>
+<tr>
+<th><?php _e('Location', 'empslight-com'); ?></th>
+
+<td><?php _e(' Location accepts multiple values like tags', 'empslight-com'); ?>. <?php _e('Location does not have a default value', 'empslight-com'); ?>.<div class="taxdef-block"><p><?php _e('There are no preset values for <b>Location:</b>', 'empslight-com'); ?></p></div></td>
+</tr>
+</table>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<?php
+}
---
+++ includes/scripts.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Enqueue Scripts Functions
+ *
+ * @package EMPSLIGHT_COM
+ * @version 1.0.0
+ * @since WPAS 4.0
+ */
+if (!defined('ABSPATH')) exit;
+add_action('admin_enqueue_scripts', 'empslight_com_load_admin_enq');
+/**
+ * Enqueue style and js for each admin entity pages and settings
+ *
+ * @since WPAS 4.0
+ * @param string $hook
+ *
+ */
+function empslight_com_load_admin_enq($hook) {
+	global $typenow;
+	if ($hook == 'edit-tags.php') {
+		return;
+	}
+	if ($hook == 'toplevel_page_empslight_com' || $hook == 'spotlight_page_empslight_com_notify') {
+		wp_enqueue_script('accordion');
+		return;
+	} else if (in_array($hook, Array(
+		'spotlight_page_empslight_com_store',
+		'spotlight_page_empslight_com_designs',
+		'spotlight_page_empslight_com_support'
+	))) {
+		wp_enqueue_style('admin-tabs', EMPSLIGHT_COM_PLUGIN_URL . 'assets/css/admin-store.css');
+		return;
+	}
+	if (in_array($typenow, Array(
+		'emd_employee'
+	))) {
+		$theme_changer_enq = 1;
+		$datetime_enq = 0;
+		$date_enq = 0;
+		$sing_enq = 0;
+		$tab_enq = 0;
+		if ($hook == 'post.php' || $hook == 'post-new.php') {
+			$unique_vars['msg'] = __('Please enter a unique value.', 'empslight-com');
+			$unique_vars['app_name'] = 'empslight_com';
+			$ent_list = get_option('empslight_com_ent_list');
+			if (!empty($ent_list[$typenow])) {
+				$unique_vars['keys'] = $ent_list[$typenow]['unique_keys'];
+			}
+			wp_enqueue_script('unique_validate-js', EMPSLIGHT_COM_PLUGIN_URL . 'assets/js/unique_validate.js', array(
+				'jquery',
+				'jquery-validate'
+			) , EMPSLIGHT_COM_VERSION, true);
+			wp_localize_script("unique_validate-js", 'unique_vars', $unique_vars);
+		}
+		if ($datetime_enq == 1) {
+			wp_enqueue_script("jquery-ui-timepicker", EMPSLIGHT_COM_PLUGIN_URL . 'assets/ext/emd-meta-box/js/jqueryui/jquery-ui-timepicker-addon.js', array(
+				'jquery-ui-datepicker',
+				'jquery-ui-slider'
+			) , EMPSLIGHT_COM_VERSION, true);
+			$tab_enq = 1;
+		} elseif ($date_enq == 1) {
+			wp_enqueue_script("jquery-ui-datepicker");
+			$tab_enq = 1;
+		}
+	}
+}
+add_action('wp_enqueue_scripts', 'empslight_com_frontend_scripts');
+/**
+ * Enqueue style and js for each frontend entity pages and components
+ *
+ * @since WPAS 4.0
+ *
+ */
+function empslight_com_frontend_scripts() {
+	$dir_url = EMPSLIGHT_COM_PLUGIN_URL;
+	if (is_page()) {
+		$grid_vars = Array();
+		$local_vars['ajax_url'] = admin_url('admin-ajax.php');
+		$wpas_shc_list = get_option('empslight_com_shc_list');
+		$check_content = "";
+		if (!is_author() && !is_tax()) {
+			$check_content = get_post(get_the_ID())->post_content;
+		}
+		if (!empty($check_content) && has_shortcode($check_content, 'employee_circle_grid')) {
+			wp_enqueue_script('jquery');
+			wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+			wp_enqueue_style('employee-circle-grid-cdn', $dir_url . 'assets/css/view-employee-circle-grid.css');
+			wp_enqueue_script('employee-circle-grid-js', $dir_url . 'assets/js/employee-circle-grid.js');
+			wp_enqueue_style('allview-css', EMPSLIGHT_COM_PLUGIN_URL . '/assets/css/allview.css');
+		}
+		return;
+	}
+	if (is_single() && get_post_type() == 'emd_employee') {
+		wp_enqueue_script('jquery');
+		wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+		wp_enqueue_style('single-employee-cdn', $dir_url . 'assets/css/view-single-employee.css');
+		wp_enqueue_style('allview-css', $dir_url . '/assets/css/allview.css');
+		return;
+	}
+}
